3555b98923c191b0a70d969a805c43a6651f2036
AjayAru <ajay.arunachalam08@gmail.com>
2021-02-05 12:16:40 +0100

    Refactor fix

--- setup.py
+++ setup.py
@@ -1,24 +1,49 @@
-from distutils.core import setup
-import setuptools
+# Copyright (C) 2021-2022 Ajay Arunachalam <ajay.arunachalam08@gmail.com>
+# License: MIT, ajay.arunachalam08@gmail.com
 
-__version__ = '0.0.1'
+import pip
+import logging
+import pkg_resources
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+def _parse_requirements(file_path):
+    pip_ver = pkg_resources.get_distribution('pip').version
+    pip_version = list(map(int, pip_ver.split('.')[:2]))
+    if pip_version >= [6, 0]:
+        raw = pip.req.parse_requirements(file_path,
+                                         session=pip.download.PipSession())
+    else:
+        raw = pip.req.parse_requirements(file_path)
+    return [str(i.req) for i in raw]
+
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+try:
+    install_reqs = _parse_requirements("requirements.txt")
+except Exception:
+    logging.warning('Fail load requirements file, so using default ones.')
+    install_reqs = []
+
+__version__ = '0.0.3'
 
 def readme():
     with open('README.md', 'r', encoding='utf-8') as f:
         return f.read()
 
 
-setuptools.setup(
+setup(
     name='regressormetricgraphplot',
     version=__version__,
+    packages=["regressormetricgraphplot"],
     description='A simple package for comparing different Regression Models and Plotting with their most common evaluation metrics.',
     long_description = readme(),
     long_description_content_type="text/markdown",
     url='https://github.com/ajayarunachalam/RegressorMetricGraphPlot',
-    install_requires=[
-          'matplotlib', 'numpy', 'sklearn', 'pandas', 'scipy',
-
-      ],
+    install_requires=install_reqs,
     license='MIT',
     include_package_data=True,
     author='Ajay Arunachalam',
